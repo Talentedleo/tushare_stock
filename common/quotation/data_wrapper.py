59066315f8fdc9e9c508791cbcd94b70381e3400
Talentedleo <40957525+Talentedleo@users.noreply.github.com>
2020-07-26 20:17:03 +0800

    Remember the todo list

--- common/quotation/data_wrapper.py
+++ common/quotation/data_wrapper.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import tushare as ts
 
-from common.utils import yml_loader as config
+from common.utils import data_saver as saver
 from common.utils import date_util
+from common.utils import yml_loader as config
 
 
 class Client:
@@ -27,20 +28,28 @@ class Client:
     def get_stock_df_daily(self):
         # 单个股票日数据
         print("---- 获取股票数据(日) ----")
-        return pro.daily(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
-                         fields=self.fields)
+        df = pro.daily(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
+                       fields=self.fields)
+
+        self.save_read_cache_data('day', df)
+        return df
 
     def get_stock_df_weekly(self):
         # 单个股票周数据
         print("---- 获取股票数据(周) ----")
-        return pro.weekly(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
-                          fields=self.fields)
+        df = pro.weekly(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
+                        fields=self.fields)
+
+        self.save_read_cache_data('week', df)
+        return df
 
     def get_stock_df_monthly(self):
         # 单个股票月数据
         print("---- 获取股票数据(月) ----")
-        return pro.monthly(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
-                           fields=self.fields)
+        df = pro.monthly(ts_code=self.stock_code, start_date=self.start_date, end_date=self.end_date,
+                         fields=self.fields)
+        self.save_read_cache_data('month', df)
+        return df
 
     def get_index_df_daily(self):
         # 日指数
@@ -51,8 +60,10 @@ class Client:
         else:
             index_code = '399001.SZ'
 
-        return pro.index_daily(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
-                               fields=self.fields)
+        df = pro.index_daily(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
+                             fields=self.fields)
+        self.save_read_cache_data('day', df)
+        return df
 
     def get_index_df_weekly(self):
         # 周指数
@@ -63,8 +74,10 @@ class Client:
         else:
             index_code = '399001.SZ'
 
-        return pro.index_weekly(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
-                                fields=self.fields)
+        df = pro.index_weekly(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
+                              fields=self.fields)
+        self.save_read_cache_data('week', df)
+        return df
 
     def get_index_df_monthly(self):
         # 月指数
@@ -75,5 +88,20 @@ class Client:
         else:
             index_code = '399001.SZ'
 
-        return pro.index_monthly(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
-                                 fields=self.fields)
+        df = pro.index_monthly(ts_code=index_code, start_date=self.start_date, end_date=self.end_date,
+                               fields=self.fields)
+        self.save_read_cache_data('month', df)
+        return df
+
+    """
+    如果文件存在就读取已有的数据, 如果没有, 就缓存起来
+    """
+    # todo 
+    def save_read_cache_data(self, date, df):
+        file_name = saver.get_csv_name(date, self.stock_code, self.start_date, self.end_date)
+        if saver.check_file_existed(file_name):
+            print('---- 读取csv数据 ----')
+            saver.read_from_csv(file_name)
+        else:
+            print('---- 保存csv数据 ----')
+            saver.save_csv(df, file_name)
---
+++ common/utils/data_saver.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+# 获取当前项目根目录
+project_path = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0] + '/../../'
+base_dir = os.path.abspath(project_path + '/storage')
+
+
+def save_csv(df, file_name):
+    file_path = base_dir + file_name
+    df.to_csv(file_path)
+
+
+def read_from_csv(file_name):
+    file_path = base_dir + file_name
+    return pd.read_csv(file_path)
+
+
+def get_csv_name(date, stock_code, start_date, end_date):
+    return '/' + date + '/' + stock_code.replace('.', '') + '_' + start_date + '_' + end_date + '.csv'
+
+
+def check_file_existed(file_name):
+    return os.path.exists(base_dir + file_name)
+
+
+if __name__ == '__main__':
+    print(base_dir)
